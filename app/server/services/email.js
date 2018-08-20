60220760fd4a0a47098a87a8483c7997d44e9f51
Rene Garcia <rene.saaenz@gmail.com>
2018-08-19 22:28:07 -0500

    Removes confirmation date

--- app/server/services/email.js
+++ app/server/services/email.js
@@ -199,7 +199,7 @@ controller.sendUserAdmitted = function(email){
 
   var locals = {
     title: 'Congratulations!',
-    body: 'You have been admited to 2018 HackMTY. Please log in into your account and complete your profile information. You have until August 19 to confirm you assistance.',
+    body: 'You have been admited to 2018 HackMTY. Please log in into your account and complete your profile information.',
   };
 
   /**
