38041c7f3a31f8a7477c7b81c99e40053ab8610f
karla <k.rbandala@gmail.com>
2019-07-20 20:53:32 -0500

    Additional validation for scripts when testing

--- scripts/getVerifyToken.js
+++ scripts/getVerifyToken.js
@@ -1,21 +1,26 @@
 require('dotenv').load();
 var mongoose        = require('mongoose');
-var database        = process.env.DATABASE || { url: "mongodb://localhost:27017"};
+var database        = process.env.DATABASE || "mongodb://localhost:27017";
 var jwt             = require('jsonwebtoken');
-mongoose.connect(database.url);
+mongoose.connect(database);
 
 var User = require('../app/server/models/User');
 
-var email = 'hacker@school.edu';
+var email = 'hacker1@school.edu';
 
 User.findOne({
   email: email
 }, function(err, user){
-  console.log(user.generateEmailVerificationToken());
-  console.log(user.generateAuthToken());
+  if(user) {
+    console.log(user.generateEmailVerificationToken());
+    console.log(user.generateAuthToken());
 
-  var temp = user.generateTempAuthToken();
-  console.log(temp);
+    var temp = user.generateTempAuthToken();
+    console.log(temp);
 
-  console.log(jwt.verify(temp, process.env.JWT_SECRET));
+    console.log(jwt.verify(temp, process.env.JWT_SECRET));
+  }
+  else {
+    console.log("No such user found");
+  }
 });
