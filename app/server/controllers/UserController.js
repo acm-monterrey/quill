3411e73decc456ccc13b982eb1fab9fa25468935
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-07-24 10:56:43 -0500

    Adds search users by tableNumbers

--- app/server/controllers/UserController.js
+++ app/server/controllers/UserController.js
@@ -228,6 +228,7 @@ UserController.getPage = function(query, callback){
     queries.push({ 'profile.name': re });
     queries.push({ 'teamCode': re });
     queries.push({ 'profile.school': re });
+    queries.pushh({ 'status.tableNumber': re})
 
     findQuery.$or = queries;
   }
